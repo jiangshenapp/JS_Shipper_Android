c0988e6c74776d92e322aa9e7847c49922a10be7
huyg <742315209@qq.com>
2019-04-24 14:04:11 +0800

    update

---
+++ app/src/main/java/com/js/driver/ui/main/adapter/FindOrderAdapter.java
@@ -0,0 +1,23 @@
+package com.js.driver.ui.main.adapter;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+import java.util.List;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Created by huyg on 2019/4/24.
+ */
+public class FindOrderAdapter extends BaseQuickAdapter<Object, BaseViewHolder> {
+
+    public FindOrderAdapter(int layoutResId, @Nullable List<Object> data) {
+        super(layoutResId, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Object item) {
+
+    }
+}
--- app/src/main/java/com/js/driver/ui/main/fragment/FindOrderFragment.java
+++ app/src/main/java/com/js/driver/ui/main/fragment/FindOrderFragment.java
@@ -4,14 +4,31 @@ import com.js.driver.App;
 import com.js.driver.R;
 import com.js.driver.di.componet.DaggerFragmentComponent;
 import com.js.driver.di.module.FragmentModule;
+import com.js.driver.ui.main.adapter.FindOrderAdapter;
 import com.js.driver.ui.main.presenter.FindOrderPresenter;
 import com.js.driver.ui.main.presenter.contract.FindOrderContract;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.xlgcx.frame.view.BaseFragment;
+
+import java.util.ArrayList;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
 
 /**
  * Created by huyg on 2019/4/1.
  */
 public class FindOrderFragment extends BaseFragment<FindOrderPresenter> implements FindOrderContract.View {
+
+
+    @BindView(R.id.recycler)
+    RecyclerView mRecycler;
+    @BindView(R.id.refresh)
+    SmartRefreshLayout mRefresh;
+
+
+    private FindOrderAdapter mAdapter;
 
     public static FindOrderFragment newInstance() {
         return new FindOrderFragment();
@@ -28,11 +45,29 @@ public class FindOrderFragment extends BaseFragment<FindOrderPresenter> implemen
 
     @Override
     protected int getLayoutId() {
-        return R.layout.fragment_service;
+        return R.layout.fragment_find_order;
     }
 
     @Override
     protected void init() {
+        initView();
+    }
+
+    private void initView() {
+        initRecycler();
+        initRefresh();
+    }
+
+    private void initRecycler() {
+        mAdapter = new FindOrderAdapter(R.layout.item_home_order,new ArrayList<>());
+        mRecycler.setLayoutManager(new LinearLayoutManager(mContext));
+        mRecycler.setAdapter(mAdapter);
+    }
+
+    private void initRefresh() {
 
     }
+
+
+
 }
