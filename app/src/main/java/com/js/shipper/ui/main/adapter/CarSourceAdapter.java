9f59448aee13ca4896ed344ebce28db8f12bad29
hanzhanbing <1655661337@qq.com>
2019-06-20 17:22:09 +0800

    发货  支付方式逻辑

--- app/src/main/java/com/js/shipper/ui/main/adapter/CarSourceAdapter.java
+++ app/src/main/java/com/js/shipper/ui/main/adapter/CarSourceAdapter.java
@@ -1,5 +1,8 @@
 package com.js.shipper.ui.main.adapter;
 
+import android.text.TextUtils;
+import android.view.TextureView;
+
 import com.baidu.mapapi.model.LatLng;
 import com.baidu.mapapi.utils.DistanceUtil;
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -26,11 +29,20 @@ public class CarSourceAdapter extends BaseQuickAdapter<LineBean, BaseViewHolder>
 
     @Override
     protected void convert(BaseViewHolder helper, LineBean item) {
+
+        String info = "";
+        if (!TextUtils.isEmpty(item.getDriverName())) {
+            info += item.getDriverName();
+        }
+        if (!TextUtils.isEmpty(item.getCarLengthName())) {
+            info += " " + item.getCarLengthName();
+        }
+        if (!TextUtils.isEmpty(item.getCarModelName())) {
+            info += "/" + item.getCarModelName();
+        }
         helper.setText(R.id.item_send_address, item.getStartAddressCodeName())
                 .setText(R.id.item_arrive_address, item.getArriveAddressCodeName())
-                .setText(R.id.item_driver_info, item.getDriverName() + " "
-                        + item.getCarLengthName() + "/"
-                        + item.getCarModelName());
+                .setText(R.id.item_driver_info, info);
 //        double distance = DistanceUtil.getDistance(mGson.fromJson(mSendShip.getPosition(), LatLng.class), mGson.fromJson(mEndShip.getPosition(), LatLng.class));
 //        helper.setText(R.id.item_distance,"总里程" + (distance > 1000 ? df.format(distance / 1000) + " Km" : ((int) distance) + "米");)
 //                .setText(R.id.item_time, TimeUtils.format(item.getti))
