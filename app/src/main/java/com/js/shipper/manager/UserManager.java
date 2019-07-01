980348afe93413e2cdaa510f56bbba807f65657f
hanzhanbing <1655661337@qq.com>
2019-07-01 15:18:06 +0800

    我的钱包判断认证状态

--- app/src/main/java/com/js/shipper/manager/UserManager.java
+++ app/src/main/java/com/js/shipper/manager/UserManager.java
@@ -3,11 +3,14 @@ package com.js.shipper.manager;
 import android.app.Activity;
 import android.content.Context;
 import android.text.TextUtils;
+import android.view.View;
 
 import com.js.shipper.App;
 import com.js.shipper.model.event.UserStatusChangeEvent;
 import com.js.shipper.ui.main.activity.MainActivity;
 import com.js.shipper.ui.user.activity.LoginActivity;
+import com.js.shipper.util.DataCleanManager;
+import com.js.shipper.widget.dialog.AppDialogFragment;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -47,7 +50,12 @@ public class UserManager {
     /**
      * 判断认证状态
      */
-    public boolean isVerified(Activity activity) {
-        return true;
+    public boolean isVerified() {
+        if (App.getInstance().personConsignorVerified != 2
+                && App.getInstance().companyConsignorVerified != 2 ) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
