f6f5884c9ec1bc1ac2fe96075d4b196f791d4d81
hanzhanbing <1655661337@qq.com>
2019-09-08 22:57:51 +0800

    包名修改

--- app/src/main/java/com/js/shipper/ui/main/fragment/DeliveryFragment.java
+++ app/src/main/java/com/js/shipper/ui/main/fragment/DeliveryFragment.java
@@ -12,10 +12,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.baidu.mapapi.model.LatLng;
 import com.base.frame.view.BaseFragment;
-import com.base.shipper.R;
+import com.js.shipper.R;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.google.gson.Gson;
 import com.js.shipper.App;
+import com.js.shipper.R;
 import com.js.shipper.di.componet.DaggerFragmentComponent;
 import com.js.shipper.di.module.FragmentModule;
 import com.js.shipper.global.Const;
