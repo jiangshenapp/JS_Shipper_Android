e2cacf114385c8b26024a87444723e1bbb4c67b3
huyg <742315209@qq.com>
2019-11-13 21:19:43 +0800

    test

--- driver/src/main/java/com/js/driver/api/FileApi.java
+++ driver/src/main/java/com/js/driver/api/FileApi.java
@@ -16,7 +16,7 @@ public interface FileApi {
 
 
     @Multipart
-    @POST("https://gateway.jiangshen56.com/admin/file/upload")
+    @POST("https://testway.jiangshen56.com/admin/file/upload")
     Observable<HttpResponse<String>> upload(@Part("resourceId")RequestBody body,
                                     @Part MultipartBody.Part file);
 
--- lib.frame/src/main/java/com/base/frame/global/Const.java
+++ lib.frame/src/main/java/com/base/frame/global/Const.java
@@ -8,6 +8,6 @@ public class Const {
     public static final String BUGLY_APP_SHIPPER_ID = "625c09c5ae";
     public static final String BUGLY_APP_DRIVER_ID = "cc472b1050";
 
-    public static String IMG_URL = "https://gateway.jiangshen56.com/admin/file/download?fileName=";
+    public static String IMG_URL = "https://testway.jiangshen56.com/admin/file/download?fileName=";
 
 }
