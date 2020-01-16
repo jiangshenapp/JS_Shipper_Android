d5222ac853e4f86e6813bb48909448fa061e6bab
hzb <1655661337@qq.com>
2020-01-16 22:50:47 +0800

    发货人、收货人信息页面选择街道功能添加；

--- app/src/main/java/com/js/shipper/ui/ship/activity/ShipUserInfoActivity.java
+++ app/src/main/java/com/js/shipper/ui/ship/activity/ShipUserInfoActivity.java
@@ -2,18 +2,31 @@ package com.js.shipper.ui.ship.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.TextureView;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.base.frame.view.SimpleActivity;
+import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
+import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
+import com.bigkoo.pickerview.view.OptionsPickerView;
+import com.facebook.stetho.common.LogUtil;
 import com.js.shipper.App;
 import com.js.shipper.R;
+import com.js.shipper.model.bean.AddressBean;
 import com.js.shipper.model.bean.ShipBean;
+import com.js.shipper.util.DataHelper;
 import com.js.shipper.util.RegexUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
@@ -31,11 +44,18 @@ public class ShipUserInfoActivity extends SimpleActivity {
     EditText mName;
     @BindView(R.id.phone)
     EditText mPhone;
+    @BindView(R.id.et_street)
+    EditText mStreet;
+    @BindView(R.id.ll_street)
+    LinearLayout llStreet;
+
     private int type;//0。发货；1.收货
     private String address;
     private String addressName;
     private ShipBean mShipBean;
-
+    private List<AddressBean.CityChildsBean.CountyChildsBean.StreetChildsBean> mStreetChildsBeans = new ArrayList<>();
+    private List<AddressBean> mAddressBeans;
+    private AddressBean.CityChildsBean.CountyChildsBean.StreetChildsBean mStreetChildsBean;
 
     public static void action(Activity context, int type, String address, String addressName, ShipBean shipBean) {
         Intent intent = new Intent(context, ShipUserInfoActivity.class);
@@ -54,6 +74,7 @@ public class ShipUserInfoActivity extends SimpleActivity {
     @Override
     protected void init() {
         initIntent();
+        initData();
         initView();
     }
 
@@ -64,6 +85,30 @@ public class ShipUserInfoActivity extends SimpleActivity {
         mShipBean = getIntent().getParcelableExtra("ship");
         mAddress.setText(address);
         mAddressName.setText(addressName);
+    }
+
+    private void initData() {
+        mAddressBeans = DataHelper.getAddress(this);
+        int adCode = mShipBean.getAddressCode();
+        int provinceCode = adCode / 10000;
+        int cityCode = adCode / 100;
+        for (AddressBean addressBean : mAddressBeans) {
+            if (Integer.parseInt(addressBean.getCode()) == provinceCode) {
+                for (AddressBean.CityChildsBean cityChildsBean : addressBean.getChilds()) {
+                    if (Integer.parseInt(cityChildsBean.getCode()) == cityCode) {
+                        for (AddressBean.CityChildsBean.CountyChildsBean countyChildsBean : cityChildsBean.getChilds()) {
+                            if (Integer.parseInt(countyChildsBean.getCode()) == adCode) {
+                                mStreetChildsBeans = countyChildsBean.getChilds();
+                                break;
+                            }
+                        }
+                        continue;
+                    }
+                }
+                continue;
+            }
+        }
+        LogUtil.d("城市编码 ："+adCode+"  街道信息："+mStreetChildsBeans.toString());
     }
 
     private void initView() {
@@ -87,12 +132,10 @@ public class ShipUserInfoActivity extends SimpleActivity {
                 mName.setText(mShipBean.getName());
             }
             if (TextUtils.isEmpty(mShipBean.getPhone())) {
-                mName.setText(App.getInstance().mobile);
+                mPhone.setText(App.getInstance().mobile);
             } else {
                 mPhone.setText(mShipBean.getPhone());
             }
-
-
         }
     }
 
@@ -101,13 +144,17 @@ public class ShipUserInfoActivity extends SimpleActivity {
 
     }
 
-
     @OnClick(R.id.confirm)
     public void onViewClicked() {
         String phone = mPhone.getText().toString().trim();
         String name = mName.getText().toString().trim();
+        String street = mStreet.getText().toString().trim();
         String address = mAddressDetail.getText().toString().trim();
 
+        if (TextUtils.isEmpty(street)) {
+            toast("请选择街道");
+            return;
+        }
 
         if (TextUtils.isEmpty(phone)) {
             toast("请输入手机号");
@@ -121,6 +168,7 @@ public class ShipUserInfoActivity extends SimpleActivity {
 
         if (TextUtils.isEmpty(name)) {
             toast("请输入姓名");
+            return;
         }
 
         Intent intent = new Intent();
@@ -128,5 +176,36 @@ public class ShipUserInfoActivity extends SimpleActivity {
         intent.putExtra("ship", shipBean);
         setResult(999, intent);
         finish();
+    }
+
+    @OnClick({R.id.et_street, R.id.ll_street})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.et_street: //选择街道
+            case R.id.ll_street:
+                showStreetPickerView();
+                break;
+        }
+    }
+
+    /**
+     * 展示街道选择器
+     */
+    public void showStreetPickerView() {
+        //条件选择器
+        OptionsPickerView pvOptions = new OptionsPickerBuilder(this, new OnOptionsSelectListener() {
+            @Override
+            public void onOptionsSelect(int options1, int option2, int options3 ,View v) {
+                //返回的分别是三个级别的选中位置
+                mStreetChildsBean = mStreetChildsBeans.get(options1);
+                mStreet.setText(mStreetChildsBean.getName());
+            }
+        }).build();
+        List<String> streetNames = new ArrayList<String>();
+        for (AddressBean.CityChildsBean.CountyChildsBean.StreetChildsBean streetChildsBean : mStreetChildsBeans) {
+            streetNames.add(streetChildsBean.getName());
+        }
+        pvOptions.setPicker(streetNames);
+        pvOptions.show();
     }
 }
